94c3fde87d17bc4473a9ccd323eaf61288797c51
Leandro Burgos <burgosleandro.c@gmail.com>
2025-07-28 17:57:31 -0400

    feat: first commit

---
+++ src/main/java/com/abaqus/portfolio/spring_boot_portfolio/SpringBootPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.abaqus.portfolio.spring_boot_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abaqus/portfolio/spring_boot_portfolio/SpringBootPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abaqus.portfolio.spring_boot_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
